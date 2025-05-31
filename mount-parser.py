a8c348b8c8470fe18355e5e685f68a853eeb4fa5
shlomi-code <shlomi@int0x2e.com>
2025-05-31 06:54:59 -0700

    init, creating basic strcture of the script

---
+++ mount-parser.py
@@ -0,0 +1,17 @@
+"""
+mount_parser.py is used for parsing the string ourput of the `mount` command.
+"""
+
+#imports
+import argparse
+
+
+
+def main():
+    """
+    The main function of the mount parser script.
+    """
+    pass  
+
+if __name__ == "__main__":
+    main()
